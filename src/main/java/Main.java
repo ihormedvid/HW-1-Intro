d1915f1368e559ca2be1bf3a926b52fff78c4fa6
Ihor Medvid <ihormedvid89@gmail.com>
2020-06-18 00:29:34 +0300

    create new project and added maven and create correct scructure

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        byte  b = 13;
+        short s = 123;
+        int   i = 1344;
+        long  l = 494949;
+
+        float f = 13.3f;
+        double d = 341.3;
+
+        System.out.println("s - b = " + (s - b));
+        System.out.println("i * l = " + (i * l));
+        System.out.println("d / f = " + (d / f));
+        System.out.println("d + l = " + (d + l));
+    }
+}
